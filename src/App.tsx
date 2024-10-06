37fc22a70b474b7516d7dd67cb998ee2e5808c69
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-06 18:46:36 +0500

    React Router Dom Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./routes/Home"
+import NotFound from "./routes/NotFound"
+
 function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   )
 }
 
