ed8fdf4e1c7e075ceb97ee705601df2f0fced678
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-08 11:13:40 +0500

    Responsive Button

--- src/routes/Documentation.tsx
+++ src/routes/Documentation.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import DocumentationContent from '../components/DocumentationContent';
-import sections from '../API/Sections.json'
+import sections from '../API/Sections.json';
 import Helmet from 'react-helmet';
 
 const Documentation: React.FC = () => {
@@ -21,15 +21,22 @@ const Documentation: React.FC = () => {
             <div className="flex min-h-screen lg:flex-row">
                 {/* Sidebar */}
                 <Sidebar sections={sections} isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-                <div className="flex-1 bg-gray-100 overflow-auto">
-                    {/* Burger BTN for mobile responsive */}
-                    <button
-                        className="p-4 m-4 bg-gray-800 text-white lg:hidden"
-                        onClick={toggleSidebar}
-                    >
-                        ☰
-                    </button>
-                    <DocumentationContent />
+                <div className="flex-1 bg-gray-100 overflow-auto relative">
+                    {/* Button Container */}
+                    <div className="fixed top-4 left-4 right-4 w-auto">
+                        {/* Background for the button with blur effect */}
+                        <div className="bg-gray-800 bg-opacity-10 lg:hidden backdrop-blur-md rounded-md p-2">
+                            <button
+                                className="p-4 text-white text-xl w-full"
+                                onClick={toggleSidebar}
+                            >
+                                ☰
+                            </button>
+                        </div>
+                    </div>
+                    <div className='mt-16 md:mt-0'>
+                        <DocumentationContent />
+                    </div>
                 </div>
             </div>
         </div>
